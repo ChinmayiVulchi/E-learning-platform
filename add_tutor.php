35d4ec3dbe72e0738b26e9f12b9ecc32d925cb16
Vulchi Devi Chinmayi <123306517+ChinmayiVulchi@users.noreply.github.com>
2023-10-11 14:16:58 +0530

    Update add_tutor.php

--- add_tutor.php
+++ add_tutor.php
@@ -32,7 +32,6 @@ if(isset($_POST['addtutor'])){
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <link rel="stylesheet" href="style3.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" type="image/x-icon" href="selogo.jpeg">
